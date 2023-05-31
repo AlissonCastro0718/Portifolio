bb7af86fc85f3e45fcd65069aed2a75cf348cc13
AlissonCastro0718 <AlissonCastroDeCarvalho@hotmail.com>
2023-05-30 21:31:04 -0300

    adição da lista mobile

---
+++ Portifolio_V2/script.js
@@ -0,0 +1,9 @@
+function openmenu(){
+    if(listadebotoes.style.display == 'block'){
+        listadebotoes.style.display = 'none'
+    }else{
+        listadebotoes.style.display = 'block'
+    }
+
+
+}
