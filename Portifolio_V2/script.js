8e80d4865547a7a822704387f3a9fb8eb8f47edf
AlissonCastro0718 <AlissonCastroDeCarvalho@hotmail.com>
2023-06-01 13:07:43 -0300

    terminado as mudanças do nome no header e adição da imagem do index para telas menores

--- Portifolio_V2/script.js
+++ Portifolio_V2/script.js
@@ -1,9 +1,6 @@
 function openmenu(){
     listamobile.style.display = 'block'
-    header.style.display = 'none'
 }
 function closemenu(){
     listamobile.style.display = 'none'
-    header.style.display = 'flex'
-
 }
