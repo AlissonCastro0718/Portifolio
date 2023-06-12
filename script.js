44a54b955c7ef28dd40c99f8fdda78d6f71c8053
AlissonCastro0718 <AlissonCastroDeCarvalho@hotmail.com>
2023-06-11 21:48:06 -0300

    mudança do local dos arquivos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function openmenu(){
     listamobile.style.display = 'block'
+    document.body.style.overflow = "hidden";
 }
 function closemenu(){
     listamobile.style.display = 'none'
+    document.body.style.overflow = "auto";
 }
